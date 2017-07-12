2c2a61455b43a62eba4fcc288d5e4289b963a869
Okan Okumus <okanokumuss@gmail.com>
2017-07-12 15:34:34 +0300

    Add files via upload

---
+++ C++/TowerOfHanoi.cpp
@@ -0,0 +1,39 @@
+/*tower of hanoi			author: yusuf shakeel	date: 20-12-2012
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void hanoi(int n, char beg, char aux, char end);
+
+int main() {
+	int N; // number of the disk
+	char a[] = "left";
+	char b[] = "middle";
+	char c[] = "right";
+	cout << "a: " << a << " tower\n";
+	cout << "b: " << b << " tower\n";
+	cout << "c: " << c << " tower\n";
+
+	cout << "enter the number of disk: ";
+	cin >> N;
+	cout << "minumum moves: " << pow(2,N) - 1 << endl;
+	printf("Moves: \n");
+	hanoi(N, 'a', 'b', 'c');	//N = 3 (no. of disks)  a, b, c are the three pegs
+	cin.get();
+	cin.ignore();
+	return 0;
+}//main() ends here
+
+void hanoi(int n, char beg, char aux, char end) {
+	if (n == 1) {
+		cout<<beg<<" --> "<< end<<endl;
+	}
+	else {
+		hanoi(n - 1, beg, end, aux);
+		hanoi(1, beg, aux, end);
+		hanoi(n - 1, aux, beg, end);
+	}
+}//t() ends here
