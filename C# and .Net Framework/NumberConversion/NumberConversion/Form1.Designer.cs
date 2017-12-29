2125352606b1cfe5247ea15132b4300e80d926d7
Okan Okumus <okanokumuss@gmail.com>
2017-12-29 13:56:22 +0300

    Add files via upload

---
+++ C# and .Net Framework/NumberConversion/NumberConversion/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace NumberConversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(127, 87);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(92, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "binary (2)      :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "decimal (10) :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(29, 242);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "hex (16)       :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 135);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "octal (8)       :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(22, 24);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(197, 24);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Number Conversion";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 27;
+            this.button2.Text = "Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(127, 135);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(92, 20);
+            this.textBox2.TabIndex = 28;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 186);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(93, 20);
+            this.textBox3.TabIndex = 29;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(126, 238);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(93, 20);
+            this.textBox4.TabIndex = 30;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label3.Location = new System.Drawing.Point(234, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "[0,1]";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label4.Location = new System.Drawing.Point(234, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 32;
+            this.label4.Text = "[0,1,...,6,7]";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label5.Location = new System.Drawing.Point(234, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 34;
+            this.label5.Text = "[0,1,...,E,F]";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.label6.Location = new System.Drawing.Point(234, 186);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "[0,1,...8,9]";
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(327, 343);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Number Converter";
+            this.TransparencyKey = System.Drawing.Color.Maroon;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ C# and .Net Framework/NumberConversion/NumberConversion/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace NumberConversion
+{
+    public partial class Form1 : Form
+    {
+        long BinaryNumber = 0;
+        long OctaNumber = 0;
+        long DecimalNumber = 0;
+        string HexNumber = "";
+        long i = 1;
+        long remainder = 0;
+        string str_hex_remainder = "", str_binary_remainder = "", str_octa_remainder = "";
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            // Set the start position of the form to the center of the screen.
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(0);
+            textBox2.Text = Convert.ToString(0);
+            textBox3.Text = Convert.ToString(0);
+            textBox4.Text = Convert.ToString(0);
+        }
+        // clear all texts
+        private void button1_Click(object sender, EventArgs e)
+        {
+                button1.Text = "CLEAR";
+                clear();
+            }
+        public void clear()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox1.Text = Convert.ToString(0);
+            textBox2.Text = Convert.ToString(0);
+            textBox3.Text = Convert.ToString(0);
+            textBox4.Text = Convert.ToString(0);
+            str_binary_remainder = "";
+            str_octa_remainder = "";
+            str_hex_remainder = "";
+            label3.Visible = false;
+            label4.Visible = false;
+            label6.Visible = false;
+            label5.Visible = false;
+        }
+        
+        // convert button
+        private void button2_Click(object sender, EventArgs e)
+        {           
+            //HexNumber
+            try
+            {
+                // only digit numbers can be accepted
+                BinaryNumber = Convert.ToInt64(textBox1.Text);
+                OctaNumber = Convert.ToInt64(textBox2.Text);
+                DecimalNumber = Convert.ToInt64(textBox3.Text);
+                // both alphebetic(A, B, C, D, E, F) and numeric numbers can be accepted
+                HexNumber = textBox4.Text;
+                // main loop for non zero value to convert
+                if (BinaryNumber != 0 || OctaNumber != 0 || DecimalNumber != 0 || HexNumber != "0")
+                {
+                    // only BINARY conversion is requested
+                    if (BinaryNumber != 0 && OctaNumber == 0 && DecimalNumber == 0 && HexNumber == "0")
+                    {
+                        binaryTOconvertor(BinaryNumber);
+                        label3.Visible = true;
+                    }
+                    // only OCTA conversion is requested
+                    if (BinaryNumber == 0 && OctaNumber != 0 && DecimalNumber == 0 && HexNumber == "0")
+                    {
+                        octaTOconvertor(OctaNumber);
+                        label4.Visible = true;
+                    }
+                    // only DECIMAL conversion is requested
+                    if (BinaryNumber == 0 && OctaNumber == 0 && DecimalNumber != 0 && HexNumber == "0")
+                    {
+                        decimalTOconvertor(DecimalNumber);
+                        label6.Visible = true;
+                    }
+                    // only HEX conversion is requested
+                    if (BinaryNumber == 0 && OctaNumber == 0 && DecimalNumber == 0 && HexNumber != "0")
+                    {
+                        hexTOconvertor(HexNumber);
+                        label5.Visible = true;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Wrong Type of Input","ERROR",MessageBoxButtons.OK);
+            }
+
+        }
+        // from BINARY number to other numbers
+        void binaryTOconvertor(long x)
+        {
+            long  xbin=x;
+            long xdecimal = 0;
+            remainder = 0;
+            i = 0;
+            double j;
+            // BINARY to DECIMAL
+            while (xbin != 0)
+            {
+                remainder = xbin % 10;
+                xbin = xbin / 10;
+                // binary numbers only contain 0 and 1
+                if (remainder == 0 || remainder == 1)
+                {
+                    j = Math.Pow(2,i);
+                    i = i + 1;  
+                    xdecimal = xdecimal + remainder * Convert.ToInt64(j);
+                }
+                else
+                {
+                    clear();
+                    goto next;
+                }
+            }
+            
+            textBox3.Text = Convert.ToString(xdecimal);
+            decimalTOconvertor(xdecimal);
+            // DECIMAL TO OCTA
+            textBox2.Text = Convert.ToString(str_octa_remainder);
+            // DECIMAL TO HEX
+            textBox4.Text = str_hex_remainder;
+            next:
+            //MessageBox.Show("Binary number contains only 0 and 1", "ERROR", MessageBoxButtons.OK);
+            ;
+        }
+        // from OCTA number to other numbers
+        void octaTOconvertor(long x)
+        {
+            long xocta = x;
+            long xdecimal = 0;
+            remainder = 0;
+            i = 0;
+            double j;
+            // OCTAL to DECIMAL
+            while (xocta != 0)
+            {
+                remainder = xocta % 10;
+                xocta = xocta / 10;
+                if (remainder >= 8)
+                {
+                    clear();
+                    goto next;
+                }
+                j = Math.Pow(8, i);
+                i = i + 1;
+                xdecimal = xdecimal + remainder * Convert.ToInt64(j);
+            }
+
+            textBox3.Text = Convert.ToString(xdecimal);
+            decimalTOconvertor(xdecimal);
+            // DECIMAL TO OCTA
+            textBox1.Text = Convert.ToString(str_binary_remainder);
+            // OCTAL to OCTAL
+            textBox2.Text = Convert.ToString(x);
+            // DECIMAL TO HEX
+            textBox4.Text = str_hex_remainder;
+            next:
+            ;
+        }
+
+        // from DECIMAL number to other numbers
+        void decimalTOconvertor(long x)
+        {
+            long xbin = x, xocta = x, xhex = x;
+            remainder=0;
+            i = 1;
+            // DECIMAL TO BINARY
+            while (xbin != 0)
+            {
+                remainder = xbin % 2; // mode 2 of x
+                xbin = xbin / 2;
+                str_binary_remainder = Convert.ToString(remainder) + str_binary_remainder;
+            }
+            // DECIMAL TO OCTAL
+            i = 1;
+            remainder = 0;
+            while (xocta != 0)
+            {
+                remainder = xocta % 8;  // mode 8 of x
+                xocta = xocta / 8;
+                str_octa_remainder = Convert.ToString(remainder) + str_octa_remainder;
+            }
+            // DECIMAL TO HEX
+            remainder = 0;
+            while (xhex != 0)
+            {
+                //str_remainder = str_remainder + str_remainder;
+                remainder = xhex % 16;  // mode 16 of x
+                if (remainder == 10)
+                {
+                    str_hex_remainder = "A" + str_hex_remainder;
+                    //str_remainder = str_remainder + str_remainder;
+                }
+                if (remainder == 11)
+                {
+                    str_hex_remainder = "B" + str_hex_remainder;
+                    //str_remainder = str_remainder + str_remainder;
+
+                }
+                if (remainder == 12)
+                {
+                    str_hex_remainder = "C" + str_hex_remainder;
+                   // str_remainder = str_remainder + str_remainder;
+
+                }
+                if (remainder == 13)
+                {
+                    str_hex_remainder = "D" + str_hex_remainder;
+                    //str_remainder = str_remainder + str_remainder;
+
+                }
+                if (remainder == 14)
+                {
+                    str_hex_remainder = "E" + str_hex_remainder;
+                    // str_remainder = str_remainder + str_remainder;
+
+                }
+                if (remainder == 15)
+                {
+                    str_hex_remainder = "F" + str_hex_remainder;
+                    //str_remainder = str_remainder + str_remainder;
+
+                }
+                if (remainder >= 0 && remainder < 10)
+                {
+                    str_hex_remainder = Convert.ToString(remainder) + str_hex_remainder;
+                    //str_remainder = str_remainder + str_remainder;
+                }
+                xhex = xhex / 16;
+
+            }
+            // DECIMAL TO BINARY
+            textBox1.Text = str_binary_remainder;
+            // DECIMAL TO OCTA
+            textBox2.Text = Convert.ToString(str_octa_remainder);
+            // DECIMAL TO HEX
+            textBox4.Text = str_hex_remainder;
+        }
+        // from HEX number to other numbers
+        void hexTOconvertor(string x)
+        {
+            string str_remainder;
+            int input_length = x.Length, i = 0;
+            long xdecimal = 0;
+            while (input_length != 0)
+            {
+                str_remainder = Convert.ToString(x[input_length-1]);
+                try
+                {
+                    remainder = Convert.ToInt64(str_remainder);
+                }
+                catch (Exception)
+                {
+                    if (str_remainder == "A" || str_remainder == "a")
+                    {
+                        remainder = 10;
+                    }
+                    else if (str_remainder == "B" || str_remainder == "b")
+                    {
+                        remainder = 11;
+                    }
+                    else if (str_remainder == "C" || str_remainder == "c")
+                    {
+                        remainder = 12;
+                    }
+                    else if (str_remainder == "D" || str_remainder == "d")
+                    {
+                        remainder = 13;
+                    }
+                    else if (str_remainder == "E" || str_remainder == "e")
+                    {
+                        remainder = 14;
+                    }
+                    else if (str_remainder == "F" || str_remainder == "f")
+                    {
+                        remainder = 15;
+                    }
+                    else
+                    {
+                        clear();
+                        goto next;
+                    }
+                }
+                
+                input_length = input_length - 1;
+                xdecimal = xdecimal + remainder * Convert.ToInt64(Math.Pow(16, i));
+                i = i + 1;
+            }
+            // DECIMAL TO HEX
+            textBox3.Text = Convert.ToString(xdecimal);
+            decimalTOconvertor(xdecimal);
+            // HEX TO BINARY
+            textBox1.Text = Convert.ToString(str_binary_remainder);
+            // HEX to OCTAL
+            textBox2.Text = Convert.ToString(str_octa_remainder);
+            // HEX TO HEX
+            textBox4.Text = Convert.ToString(x);
+            next:
+            ;
+        }
+    }
+}
