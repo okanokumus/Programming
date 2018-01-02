cd0cfb21f57c665e30017796518b9b67f4d797fe
Okan Okumus <okanokumuss@gmail.com>
2018-01-02 10:24:01 +0300

    Add files via upload

---
+++ C# and .Net Framework/Taş Kağıt Makas/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace TaşKağıtMakas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(27, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1. Oyuncu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(216, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2. Oyuncu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 113);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(112, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(216, 113);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(112, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(256, 173);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(49, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(38, 173);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(49, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(116, 188);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Sıfırla";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Wheat;
+            this.label1.Location = new System.Drawing.Point(33, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 18);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Oyun Kaçta Biter?";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(193, 12);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(49, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.TextChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(116, 159);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Sonuç";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Olive;
+            this.ClientSize = new System.Drawing.Size(354, 245);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "Taş Kağıt Makas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ C# and .Net Framework/Taş Kağıt Makas/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace TaşKağıtMakas
+{
+    public partial class Form1 : Form
+    {
+        int indeks1, indeks2; // taş kağıt makasın indeksleri
+        int ctr1, ctr2; // kullanıcı puanlarını tutar
+        int sınır = 0; // oyunun kaçta biteceğini belirler
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox3.Text = "0";
+            textBox4.Text = "0";
+            textBox5.Text = "0";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            label1.Text = "Oyun Kaçta Biter?";
+            sınır = Convert.ToInt32(textBox5.Text);
+        }
+        // 1. oyuncu sonuç üretici
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string[] seçenekler = { "TAŞ", "KAĞIT", "MAKAS" };
+            // random indeksler üretilir
+            indeks1 = rnd.Next(0, seçenekler.Length);
+            textBox1.Text = seçenekler[indeks1];
+            
+        }
+        // 2. oyuncu sonuç üretici
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            string[] seçenekler = { "TAŞ", "KAĞIT", "MAKAS" };
+            // random indeksler üretilir
+            indeks2 = rnd.Next(0, seçenekler.Length);
+            textBox2.Text = seçenekler[indeks2];
+            
+        }
+        // skorları sıfırla
+        public void clear()
+        {
+            ctr1 = 0;
+            ctr2 = 0;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = Convert.ToString(ctr2);
+            textBox4.Text = Convert.ToString(ctr1);
+            textBox5.Text = "0";
+        }
+        // sıfırla butonu
+        private void button4_Click(object sender, EventArgs e)
+        {
+            clear();
+            textBox5.Text = "0";
+        }
+       
+        // skorları hesaplar
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+            if (sınır == 0)
+            {
+                MessageBox.Show("Lütfen Oyunun Biteceği Sınırı Giriniz", "UYARI", MessageBoxButtons.OK);
+            }
+            else
+            {
+                okan();
+                sonucHesap();
+            }
+            
+        }
+        public void sonucHesap()
+        {
+            DialogResult dialog;
+            if (ctr1 == sınır || ctr2 == sınır)
+            {
+                dialog = MessageBox.Show("Devam etmek istiyor musunuz?\nÖnceki bilgiler sınırlanır!", "Sonuç", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+                if (dialog == DialogResult.Retry)
+                {
+                    clear();
+                }
+                else
+                {
+                    if (ctr1 > ctr2)
+                    {
+                        MessageBox.Show("1. Oyuncu Kazandı!", "Bilgi", MessageBoxButtons.OK);
+                        clear();
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("2. Oyuncu Kazandı!", "Bilgi", MessageBoxButtons.OK);
+                        clear();
+                    }
+                        
+                }
+            }
+        }
+        public void okan()
+        {
+
+            // taş taş -> 
+            if ((indeks1 == 0 && indeks2 == 0) || (indeks1 == 1 && indeks2 == 1) || (indeks1 == 2 && indeks2 == 2))
+            {
+                ctr1 = ctr1 + 0;
+                ctr2 = ctr2 + 0;
+            }
+            // taş kağıt -> kağıt
+            if (indeks1 == 0 && indeks2 == 1)
+                ctr2 = ctr2 + 1;
+            // taş makas -> taş
+            if (indeks1 == 0 && indeks2 == 2)
+                ctr1 = ctr1 + 1;
+            // kağıt taş -> kağıt
+            if (indeks1 == 1 && indeks2 == 0)
+                ctr1 = ctr1 + 1;
+            // kağıt makas -> makas
+            if (indeks1 == 1 && indeks2 == 2)
+                ctr2 = ctr2 + 1;
+            // makas taş -> taş
+            if (indeks1 == 2 && indeks2 == 0)
+                ctr2 = ctr2 + 1;
+            // makas kağıt -> makas
+            if (indeks1 == 2 && indeks2 == 1)
+                ctr1 = ctr1 + 1;
+            textBox4.Text = Convert.ToString(ctr1);
+            textBox3.Text = Convert.ToString(ctr2);
+            //textBox4.Text = Convert.ToString(sınır);
+        }
+        // sonuçları temizle
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+    }
+}
