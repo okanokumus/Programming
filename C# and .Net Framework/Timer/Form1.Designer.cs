b4a986b9da7265955def15b1e8c5179b685a3fe3
Okan Okumus <okanokumuss@gmail.com>
2018-01-12 12:00:00 +0300

    Add files via upload

---
+++ C# and .Net Framework/Timer/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TimerShutDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START TIMER";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Normal Shut Down",
+            "Fast Shut Down Forcibly",
+            "Restart",
+            "Sleep",
+            "Hibernate"});
+            this.comboBox1.Location = new System.Drawing.Point(154, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(128, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(151, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select what you want";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(9, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Time ( h : m :s )";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(21, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(22, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(99, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(20, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = ":";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = ":";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 92);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 27);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(292, 145);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Timer ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ C# and .Net Framework/Timer/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace TimerShutDown
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        public int hour;
+        public int min;
+        public int sec;
+        public int total_sec = 0 ;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+
+        }
+        // START the timer
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hour = Convert.ToInt32(textBox1.Text);
+            min = Convert.ToInt32(textBox2.Text);
+            sec = Convert.ToInt32(textBox3.Text);
+            total_sec = hour * 3600 + min * 60 + sec;
+            if (hour!=0 || min != 0 || sec!=0)
+            {
+                timer.Interval = 1000; // Time Interval 1000 miliseconds = 1 sec
+                timer.Tick += new EventHandler(timer1_Tick); //Raising Event on each tick
+                timer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please Enter The Time for Timer to Action", "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+        // select from choices that may computer to do
+        private void SelectChoice()
+        {
+            if (comboBox1.Text == "Normal Shut Down")
+            {
+                System.Diagnostics.Process.Start("shutdown", "/s /t 0");
+            }
+            else if (comboBox1.Text == "Fast Shut Down Forcibly")
+            {
+                System.Diagnostics.Process.Start("shutdown", "/s /f /t 0");
+            }
+            else if (comboBox1.Text == "Restart")
+            {
+                System.Diagnostics.Process.Start("shutdown", "/r /t 0");
+            }
+            else if (comboBox1.Text == "Sleep")
+            {
+                Application.SetSuspendState(PowerState.Suspend, true, true);
+            }
+            else if (comboBox1.Text == "Hibernate")
+            {
+                Application.SetSuspendState(PowerState.Hibernate, true, true);
+            }
+            else
+            {
+                MessageBox.Show("Unknown Action for Computer","WARNING",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (total_sec > 0)
+            {
+                total_sec = total_sec - 1;
+                hour = total_sec / 3600; // Left hours
+                min = (total_sec - (hour * 3600)) / 60; //Left Minutes
+                sec = total_sec - (hour * 3600) - (min * 60); //Left Seconds
+                textBox1.Text = hour.ToString(); // Setting hour Text on each timer tick
+                textBox2.Text = min.ToString(); // Setting minutes Text on each timer tick
+                textBox3.Text = sec.ToString(); // Setting sec Text on each timer tick
+            }
+            else
+            {
+                timer.Stop();
+                SelectChoice();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            textBox3.Text = "0";
+        }
+    }
+}
